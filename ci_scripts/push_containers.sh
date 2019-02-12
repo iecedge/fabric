cc961bb5a51e7c8f1426cc41fd6161e909d047c5
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-02-12 08:49:03 -0400

    Point to correct Dockerfile

--- ci_scripts/push_containers.sh
+++ ci_scripts/push_containers.sh
@@ -2,5 +2,5 @@ export IMAGE_TAG=$(cat VERSION)
 export AARCH=`uname -m`
 export IMAGE_NAME=fabric-synchronizer
 
-docker build -t cachengo/$IMAGE_NAME-$AARCH:$IMAGE_TAG .
+docker build -t cachengo/$IMAGE_NAME-$AARCH:$IMAGE_TAG -f Dockerfile.synchronizer .
 docker push cachengo/$IMAGE_NAME-$AARCH:$IMAGE_TAG
